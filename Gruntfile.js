41628ef732a1c2d35f5525a40745f7ed106cc6bb
Krzysztof <k.biskup.ns@gmail.com>
2017-06-28 20:30:14 +0200

    pluginy grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-  	sass: {
+    sass: {
       options: {
         sourceMap: true
       },
@@ -11,11 +11,49 @@ module.exports = function(grunt) {
           'style.css': 'styles.sass'
         }
       }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'img/build/'
+        }]
+      }
+    },
+
+    watch: {
+            files: 'app/scss/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'style.css': 'styles.scss'
+                }
+            }
+        },
+
+    browserSync: {
+    bsFiles: {
+        src : '*.css'
+    },
+    options: {
+        server: {
+            baseDir: "./"
+        }
     }
+}
 
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
 };
