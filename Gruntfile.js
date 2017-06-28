9a237ccdd35c9bc85da1ca004f8af51fc0b26654
Krzysztof <k.biskup.ns@gmail.com>
2017-06-28 18:52:42 +0200

    dodany Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+  });
+  // Load the plugins tasks
+
+  // Default task(s).
+  grunt.registerTask('default', []);
+};
